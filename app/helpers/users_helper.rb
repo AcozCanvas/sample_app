872786473e0a92d23b3b3df1d8ffaaa6928821f5
AcozCanvas <acostanzo@canvas.is>
2012-07-18 11:26:54 -0400

    Compelted chapeter 7 exorcises

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,11 @@
 module UsersHelper
   # Return the Gravatar (http://gravatar.com/) for the given user
-  def gravatar_for(user)
+  def gravatar_for(user, size=80)
+    size = 1 if size < 1
+    size = 512 if size > 512
+
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,6 +28,26 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector 'title', text: 'Sign up' }
+        it { should have_selector '#error_explanation .alert-error', text: 'error' }
+      end
+
+      describe "with invalid password" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: ""
+          fill_in "Confirmation", with: ""
+          click_button submit
+        end
+
+        it { should have_selector '#error_explanation', text: "Password can't be blank" }
+        it { should_not have_selector '#error_explanation', text: "Password digest" }
+      end
     end
 
     describe "with valid information" do
@@ -40,6 +60,13 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after submisstion" do
+        before { click_button submit }
+
+        it { should have_selector 'title', text: full_title("Example User") }
+        it { should have_selector '.alert.alert-success', text: "Welcome to the Sample App!" }
       end
     end
   end
